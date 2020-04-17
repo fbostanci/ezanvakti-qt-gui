36d75e2f0f31d430c54dde8c429ae5d03565c0c8
fbostanci <ironic@yaani.com>
2020-04-18 02:13:43 +0300

    Yalnızca bir örnek çalısması

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include "qtezanvakti.h"
 
 #include <QApplication>
+#include <QSharedMemory>
 
 int main(int argc, char *argv[])
 {
+    QSharedMemory _singular("QtkapatInstance");
+    if(_singular.attach(QSharedMemory::ReadOnly))
+    {
+        _singular.detach();
+        return -42;
+    } else
+        _singular.create(1);
     QApplication a(argc, argv);
     QtEzanvakti w;
     w.show();
